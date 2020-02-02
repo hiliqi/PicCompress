75993aac9c1dec6a0a5ca11599b005ee397b1f81
guoguo <2354687725@qq.com>
2020-02-02 19:47:50 +0800

    1

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtWaterMark = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.coY = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
@@ -44,39 +46,81 @@
             this.btnSrc = new System.Windows.Forms.Button();
             this.btnOutput = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnWaterImg = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.fontSize = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.opacity = new System.Windows.Forms.NumericUpDown();
+            this.fontColor = new System.Windows.Forms.ListBox();
+            this.lable11 = new System.Windows.Forms.Label();
+            this.scale = new System.Windows.Forms.NumericUpDown();
+            this.lbWaterMark = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.coY)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.coX)).BeginInit();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.quality)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.compressRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fontSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.opacity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scale)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.scale);
+            this.groupBox1.Controls.Add(this.lable11);
+            this.groupBox1.Controls.Add(this.fontColor);
+            this.groupBox1.Controls.Add(this.opacity);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.fontSize);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.btnWaterImg);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtWaterMark);
+            this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.coY);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.coX);
             this.groupBox1.Location = new System.Drawing.Point(36, 22);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(517, 98);
+            this.groupBox1.Size = new System.Drawing.Size(517, 548);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "水印坐标";
+            this.groupBox1.Text = "水印设置";
+            // 
+            // txtWaterMark
+            // 
+            this.txtWaterMark.Location = new System.Drawing.Point(91, 111);
+            this.txtWaterMark.Name = "txtWaterMark";
+            this.txtWaterMark.Size = new System.Drawing.Size(376, 21);
+            this.txtWaterMark.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "水印文字";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(278, 42);
+            this.label2.Location = new System.Drawing.Point(21, 74);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.Size = new System.Drawing.Size(59, 12);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Y";
+            this.label2.Text = "水印坐标Y";
             // 
             // coY
             // 
-            this.coY.Location = new System.Drawing.Point(295, 40);
+            this.coY.Location = new System.Drawing.Point(91, 72);
             this.coY.Name = "coY";
             this.coY.Size = new System.Drawing.Size(164, 21);
             this.coY.TabIndex = 3;
@@ -84,15 +128,15 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 42);
+            this.label1.Location = new System.Drawing.Point(21, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.Size = new System.Drawing.Size(59, 12);
             this.label1.TabIndex = 2;
-            this.label1.Text = "X";
+            this.label1.Text = "水印坐标X";
             // 
             // coX
             // 
-            this.coX.Location = new System.Drawing.Point(68, 40);
+            this.coX.Location = new System.Drawing.Point(91, 33);
             this.coX.Name = "coX";
             this.coX.Size = new System.Drawing.Size(164, 21);
             this.coX.TabIndex = 1;
@@ -103,7 +147,7 @@
             this.groupBox2.Controls.Add(this.quality);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.compressRate);
-            this.groupBox2.Location = new System.Drawing.Point(36, 152);
+            this.groupBox2.Location = new System.Drawing.Point(609, 23);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(517, 98);
             this.groupBox2.TabIndex = 1;
@@ -125,6 +169,11 @@
             this.quality.Name = "quality";
             this.quality.Size = new System.Drawing.Size(92, 21);
             this.quality.TabIndex = 5;
+            this.quality.Value = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
             // 
             // label3
             // 
@@ -145,7 +194,7 @@
             // lbSrc
             // 
             this.lbSrc.AutoSize = true;
-            this.lbSrc.Location = new System.Drawing.Point(34, 371);
+            this.lbSrc.Location = new System.Drawing.Point(607, 289);
             this.lbSrc.Name = "lbSrc";
             this.lbSrc.Size = new System.Drawing.Size(41, 12);
             this.lbSrc.TabIndex = 6;
@@ -154,7 +203,7 @@
             // lbOutput
             // 
             this.lbOutput.AutoSize = true;
-            this.lbOutput.Location = new System.Drawing.Point(34, 405);
+            this.lbOutput.Location = new System.Drawing.Point(607, 323);
             this.lbOutput.Name = "lbOutput";
             this.lbOutput.Size = new System.Drawing.Size(53, 12);
             this.lbOutput.TabIndex = 7;
@@ -163,7 +212,7 @@
             // lbLog
             // 
             this.lbLog.AutoSize = true;
-            this.lbLog.Location = new System.Drawing.Point(34, 439);
+            this.lbLog.Location = new System.Drawing.Point(607, 357);
             this.lbLog.Name = "lbLog";
             this.lbLog.Size = new System.Drawing.Size(53, 12);
             this.lbLog.TabIndex = 8;
@@ -171,7 +220,7 @@
             // 
             // btnSrc
             // 
-            this.btnSrc.Location = new System.Drawing.Point(95, 282);
+            this.btnSrc.Location = new System.Drawing.Point(668, 153);
             this.btnSrc.Name = "btnSrc";
             this.btnSrc.Size = new System.Drawing.Size(94, 23);
             this.btnSrc.TabIndex = 0;
@@ -181,7 +230,7 @@
             // 
             // btnOutput
             // 
-            this.btnOutput.Location = new System.Drawing.Point(195, 282);
+            this.btnOutput.Location = new System.Drawing.Point(768, 153);
             this.btnOutput.Name = "btnOutput";
             this.btnOutput.Size = new System.Drawing.Size(104, 23);
             this.btnOutput.TabIndex = 1;
@@ -191,7 +240,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(399, 282);
+            this.button1.Location = new System.Drawing.Point(972, 153);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -199,11 +248,151 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 382);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "水印图片";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnWaterImg
+            // 
+            this.btnWaterImg.Location = new System.Drawing.Point(91, 377);
+            this.btnWaterImg.Name = "btnWaterImg";
+            this.btnWaterImg.Size = new System.Drawing.Size(91, 23);
+            this.btnWaterImg.TabIndex = 8;
+            this.btnWaterImg.Text = "选择水印图片";
+            this.btnWaterImg.UseVisualStyleBackColor = true;
+            this.btnWaterImg.Click += new System.EventHandler(this.btnWaterImg_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "文字大小";
+            // 
+            // fontSize
+            // 
+            this.fontSize.Location = new System.Drawing.Point(91, 150);
+            this.fontSize.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.fontSize.Name = "fontSize";
+            this.fontSize.Size = new System.Drawing.Size(164, 21);
+            this.fontSize.TabIndex = 10;
+            this.fontSize.Value = new decimal(new int[] {
+            28,
+            0,
+            0,
+            0});
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 195);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "文字颜色";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(15, 340);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(65, 12);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "水印透明度";
+            // 
+            // opacity
+            // 
+            this.opacity.Location = new System.Drawing.Point(91, 338);
+            this.opacity.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.opacity.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.opacity.Name = "opacity";
+            this.opacity.Size = new System.Drawing.Size(164, 21);
+            this.opacity.TabIndex = 18;
+            this.opacity.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // fontColor
+            // 
+            this.fontColor.FormattingEnabled = true;
+            this.fontColor.ItemHeight = 12;
+            this.fontColor.Items.AddRange(new object[] {
+            "红色",
+            "白色",
+            "黑色"});
+            this.fontColor.Location = new System.Drawing.Point(91, 195);
+            this.fontColor.Name = "fontColor";
+            this.fontColor.Size = new System.Drawing.Size(120, 88);
+            this.fontColor.TabIndex = 19;
+            // 
+            // lable11
+            // 
+            this.lable11.AutoSize = true;
+            this.lable11.Location = new System.Drawing.Point(39, 301);
+            this.lable11.Name = "lable11";
+            this.lable11.Size = new System.Drawing.Size(41, 12);
+            this.lable11.TabIndex = 20;
+            this.lable11.Text = "放大率";
+            // 
+            // scale
+            // 
+            this.scale.Location = new System.Drawing.Point(91, 299);
+            this.scale.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.scale.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.scale.Name = "scale";
+            this.scale.Size = new System.Drawing.Size(164, 21);
+            this.scale.TabIndex = 21;
+            this.scale.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // lbWaterMark
+            // 
+            this.lbWaterMark.AutoSize = true;
+            this.lbWaterMark.Location = new System.Drawing.Point(607, 253);
+            this.lbWaterMark.Name = "lbWaterMark";
+            this.lbWaterMark.Size = new System.Drawing.Size(53, 12);
+            this.lbWaterMark.TabIndex = 9;
+            this.lbWaterMark.Text = "水印图片";
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 562);
+            this.ClientSize = new System.Drawing.Size(1390, 781);
+            this.Controls.Add(this.lbWaterMark);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lbLog);
             this.Controls.Add(this.btnOutput);
@@ -214,7 +403,7 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Main";
+            this.Text = "超级图片压缩带水印功能";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.coY)).EndInit();
@@ -223,6 +412,9 @@
             this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.quality)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.compressRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fontSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.opacity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scale)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -246,5 +438,18 @@
         private System.Windows.Forms.Button btnSrc;
         private System.Windows.Forms.Button btnOutput;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtWaterMark;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnWaterImg;
+        private System.Windows.Forms.NumericUpDown fontSize;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown opacity;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ListBox fontColor;
+        private System.Windows.Forms.NumericUpDown scale;
+        private System.Windows.Forms.Label lable11;
+        private System.Windows.Forms.Label lbWaterMark;
     }
 }
--- Main.cs
+++ Main.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Watermark;
 
 namespace PicCompress
 {
@@ -17,6 +18,7 @@ namespace PicCompress
         private string source;
         private string output;
         private string filePath;
+        private string waterImgPath;
         public Main()
         {
             InitializeComponent();
@@ -62,7 +64,7 @@ namespace PicCompress
             source = string.Empty;
             output = string.Empty;
         }
-   
+
         private ImageCodecInfo GetEncoder(ImageFormat format)
         {
             foreach (ImageCodecInfo imageCodecInfo in ImageCodecInfo.GetImageDecoders())
@@ -75,74 +77,146 @@ namespace PicCompress
             return null;
         }
 
+
+        //private void ApplyWaterMark(string file, string dir)
+        //{
+        //    try
+        //    {
+        //        FileInfo fileInfo = new FileInfo(file);
+        //        string name = fileInfo.Name;
+        //        Image image = Image.FromFile(file);
+        //        int num = ((int)compressRate.Value == 0) ? 1 : ((int)compressRate.Value);
+        //        Bitmap bitmap = new Bitmap(image, image.Width / num, image.Height / num);
+        //        Graphics graphics = Graphics.FromImage(bitmap);
+        //        Brush brush = new SolidBrush(Color.FromArgb(1, Color.Black));
+        //        Font font = new Font(FontFamily.GenericSansSerif, 28);
+        //        int x = (int)coX.Value;
+        //        int y = (int)coY.Value;
+        //        string waterMark = txtWaterMark.Text.Trim();
+        //        if (string.IsNullOrEmpty(waterMark))
+        //        {
+        //            waterMark = ".";
+        //        }
+        //        graphics.DrawString(waterMark, font, brush, new Point(x, y));
+        //        if (!Directory.Exists(dir))
+        //        {
+        //            Directory.CreateDirectory(dir);
+        //        }
+        //        System.Drawing.Imaging.Encoder encoder = System.Drawing.Imaging.Encoder.Quality;
+        //        EncoderParameters encoderParameters = new EncoderParameters(1);
+        //        decimal value = quality.Value;
+        //        EncoderParameter encoderParameter = new EncoderParameter(encoder, (long)value);
+        //        encoderParameters.Param[0] = encoderParameter;
+        //        ImageCodecInfo encoder2 = GetEncoder(ImageFormat.Jpeg);
+        //        bitmap.Save(Path.Combine(dir, name.Replace(fileInfo.Extension, ".jpg")), encoder2, encoderParameters);
+        //        image.Dispose();
+        //        bitmap.Dispose();
+        //        graphics.Dispose();
+        //        brush.Dispose();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        File.AppendAllText("error.txt", dir + "," + ex.Message + Environment.NewLine);
+        //    }
+        //}
+
         private void ApplyWaterMark(string file, string dir)
         {
-            try
+            FileInfo fileInfo = new FileInfo(file);
+            string name = fileInfo.Name;
+            Image image = Image.FromFile(file);
+            string text = txtWaterMark.Text.Trim();
+            Watermarker objWatermarker = new Watermarker(image);
+            // Set the properties for the logo
+            objWatermarker.Position = WatermarkPosition.Absolute;
+            objWatermarker.PositionX = (int)coX.Value;
+            objWatermarker.PositionY = (int)coY.Value;
+            //objWatermarker.Margin = new Padding(20);
+            objWatermarker.Opacity = (float)opacity.Value/10;
+            objWatermarker.Font = new Font(FontFamily.GenericMonospace, (float)fontSize.Value);
+            switch (fontColor.SelectedIndex)
             {
-                FileInfo fileInfo = new FileInfo(file);
-                string name = fileInfo.Name;
-                Image image = Image.FromFile(file);
-                int num = ((int)compressRate.Value == 0) ? 1 : ((int)compressRate.Value);
-                Bitmap bitmap = new Bitmap(image, image.Width / num, image.Height / num);
-                Graphics graphics = Graphics.FromImage(bitmap);
-                Brush brush = new SolidBrush(Color.FromArgb(1, Color.LightSteelBlue));
-                Font font = new Font(FontFamily.GenericSerif, 1f);
-                int x = (int)coX.Value;
-                int y = (int)coY.Value;
-                graphics.DrawString(".", font, brush, new Point(x, y));
-                if (!Directory.Exists(dir))
-                {
-                    Directory.CreateDirectory(dir);
-                }
-                System.Drawing.Imaging.Encoder encoder = System.Drawing.Imaging.Encoder.Quality;
-                EncoderParameters encoderParameters = new EncoderParameters(1);
-                decimal value = quality.Value;
-                EncoderParameter encoderParameter = new EncoderParameter(encoder, (long)value);
-                encoderParameters.Param[0] = encoderParameter;
-                ImageCodecInfo encoder2 = GetEncoder(ImageFormat.Jpeg);
-                bitmap.Save(Path.Combine(dir, name.Replace(fileInfo.Extension, ".jpg")), encoder2, encoderParameters);
-                image.Dispose();
-                bitmap.Dispose();
-                graphics.Dispose();
-                brush.Dispose();
+                case 0:
+                    objWatermarker.FontColor = Color.DarkRed; break;
+                case 1:
+                    objWatermarker.FontColor = Color.White; break;
+                default:
+                    objWatermarker.FontColor = Color.Black; break;
             }
-            catch (Exception ex)
+
+            objWatermarker.TransparentColor = Color.White;
+            objWatermarker.ScaleRatio = (float)scale.Value;
+            // Draw the logo
+            if (!string.IsNullOrEmpty(waterImgPath))
             {
-                File.AppendAllText("error.txt", dir + "," + ex.Message + Environment.NewLine);
+                objWatermarker.DrawImage(waterImgPath);
             }
+            
+            //Draw the Text
+            if (!string.IsNullOrEmpty(text))
+            {
+                objWatermarker.DrawText(text);
+            }
+
+
+            System.Drawing.Imaging.Encoder encoder = System.Drawing.Imaging.Encoder.Quality;
+            EncoderParameters encoderParameters = new EncoderParameters(1);
+            decimal value = quality.Value;
+            EncoderParameter encoderParameter = new EncoderParameter(encoder, (long)value);
+            encoderParameters.Param[0] = encoderParameter;
+            ImageCodecInfo encoder2 = GetEncoder(ImageFormat.Jpeg);
+            objWatermarker.Image.Save(Path.Combine(dir, name.Replace(fileInfo.Extension, ".jpg")), encoder2, encoderParameters);
         }
 
         private void btnSrc_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-            folderBrowserDialog.Description = "请选择图片所在文件夹";
-            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
             {
-                if (string.IsNullOrEmpty(folderBrowserDialog.SelectedPath))
+
+                folderBrowserDialog.Description = "请选择图片所在文件夹";
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
                 {
-                    lbLog.Text = "文件夹路径不能为空";
-                    return;
+                    if (string.IsNullOrEmpty(folderBrowserDialog.SelectedPath))
+                    {
+                        lbLog.Text = "文件夹路径不能为空";
+                        return;
+                    }
+                    lbSrc.Text = lbSrc.Text + folderBrowserDialog.SelectedPath;
+                    source = folderBrowserDialog.SelectedPath;
                 }
-                lbSrc.Text = folderBrowserDialog.SelectedPath;
-                source = folderBrowserDialog.SelectedPath;
             }
         }
 
         private void btnOutput_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-            folderBrowserDialog.Description = "请选择输出文件夹";
-            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
             {
-                if (string.IsNullOrEmpty(folderBrowserDialog.SelectedPath))
+                folderBrowserDialog.Description = "请选择输出文件夹";
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
                 {
-                    lbLog.Text = "文件夹路径不能为空";
-                    return;
+                    if (string.IsNullOrEmpty(folderBrowserDialog.SelectedPath))
+                    {
+                        lbLog.Text = "文件夹路径不能为空";
+                        return;
+                    }
+                    lbOutput.Text = lbOutput.Text + folderBrowserDialog.SelectedPath;
+                    output = folderBrowserDialog.SelectedPath;
                 }
-                lbOutput.Text = folderBrowserDialog.SelectedPath;
-                output = folderBrowserDialog.SelectedPath;
             }
         }
-      
+
+        private void btnWaterImg_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fileDialog = new OpenFileDialog())
+            {
+                fileDialog.Title = "请选择文件";
+                fileDialog.Filter = "jpg文件|*.jpg|png文件|*.png|gif文件|*.gif"; //设置要选择的文件的类型
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    waterImgPath = fileDialog.FileName;//返回文件的完整路径  
+                    lbWaterMark.Text = lbWaterMark.Text + waterImgPath;
+                }
+            }
+        }
     }
 }
